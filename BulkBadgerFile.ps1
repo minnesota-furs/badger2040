84d727aafd51670e18b63253673b97206c78329a
Legonnare <legonnare@gmail.com>
2022-03-28 18:24:08 -0500

    Update BulkBadgerFile.ps1
    Added functionality to change the Role and Company name from parameter.
    Ex: .\BulkBadgerFile.ps1 -Path C:\Temp\Badge2040\Badgelist.csv -Company "Furry Migration 2022" -Job "Rocket Scientist"
    Defaults to "Furry Migration 2022" for the Company and "Rocket Scientist" for the Job.

--- BulkBadgerFile.ps1
+++ BulkBadgerFile.ps1
@@ -1,16 +1,30 @@
-param
-(
-    $Path
+Param(
+    [Parameter(HelpMessage = "What is the path to the CSV file with all the badge names and numbers?")]
+    [string]$Path,
+    [Parameter(HelpMessage = "What Company should be listed?")]
+    [string]$Company = "Furry Migration 2022",
+    [Parameter(HelpMessage = "What job title should be given?")]
+    [string]$Job = "Rocket Scientist"
 )
 $CSV = Import-Csv -Path "$($Path)"
 $SaveDir = split-path -Path $Path -Parent
-New-item -Path "$($SaveDir)\txt\" -ItemType Directory
+
+if (Test-Path -Path "$($SaveDir)\txt\")
+{
+    Remove-Item -Path "$($SaveDir)\txt\" -Recurse -Force
+    New-item -Path "$($SaveDir)\txt\" -ItemType Directory | Out-null
+}
+else
+{
+    New-item -Path "$($SaveDir)\txt\" -ItemType Directory
+}
+
 foreach ($Entry in $CSV)
 {
-    Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "Furry Migration 2022"
+    Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "$($Company)"
     Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "$($Entry.BadgeName)"
     Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "Role"
-    Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "Rocket Engineer"
+    Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "$($Job)"
     Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "Badge #"
     Add-content -Path "$($SaveDir)\txt\$($Entry.BadgeNumber)_badge.txt" -Value "$($Entry.BadgeNumber)"
 }
